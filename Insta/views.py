ba7ca20d8667e38b1dcbebcdda0d2cb50472ad3e
woshifrank <frankyyj98@gmail.com>
2020-03-08 22:13:13 -0400

    views setup complete, working on url patterns

--- Insta/views.py
+++ Insta/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+#from django.shortcuts import render
+from django.views.generic import TemplateView
+class HelloWorld(TemplateView):
+    template_name = 'test.html'
